5646a258428d53ee0da5da751fc47f1fc5c5bf9a
Aiyoun Khan <aiyounkhan@Aiyouns-Mac-mini.local>
2024-01-14 01:58:46 +0600

    Basic navbar done

--- client/src/pages/Root.jsx
+++ client/src/pages/Root.jsx
@@ -1,16 +1,20 @@
 import { Outlet } from "react-router-dom"
 import Menu from "../components/Menu"
+import Navbar from "../components/Navbar"
 
 const Root = () => {
-  return (
-    <div>
-          <Menu
-          address = 'KGG, Lamp Street, 345/A Road'
-          phone = '019312332'
-          email = 'shareplate@gmail.com'
-          ></Menu>
-          <Outlet></Outlet>
-    </div>
-  )
+    return (
+        <div>
+            <Menu
+                address='KGG, Lamp Street, 345/A Road'
+                phone='019312332'
+                email='shareplate@gmail.com'
+            ></Menu>
+            <Navbar
+                logoName='SharePlate'
+            ></Navbar>
+            <Outlet></Outlet>
+        </div>
+    )
 }
 export default Root
