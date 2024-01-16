62fb7814b287c136efe3b4cf84c68552c47c4972
Aiyoun Khan <aiyounkhan@Aiyouns-Mac-mini.local>
2024-01-17 04:02:46 +0600

    review section done

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -1,6 +1,53 @@
+import { Avatar, Rating, Typography } from "@mui/material"
+import { Box } from "@mui/system"
+import { useState } from "react";
+
+
 const Review = () => {
+
   return (
-    <div>Review</div>
+    <div>
+      <Box
+      sx={{
+        display: 'flex',
+        gap: 2
+      }}
+      >
+        <Avatar sx={{
+          bgcolor: 'red',
+          padding: 4
+        }} variant="square">
+          N
+        </Avatar>
+        <Box>
+          <Rating
+            name="simple-controlled"
+            value={3}
+          />
+          <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 2
+          }}
+          >
+            <Box
+              sx={{
+                display: 'flex',
+                gap: 1
+              }}
+            >
+              <Typography>John Doe</Typography>
+              -
+              <Typography>1.1.2012</Typography>
+            </Box>
+            <Typography>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab facilis, eligendi vel molestias quae beatae reprehenderit temporibus, fugiat quo doloremque expedita provident consequatur, eum ut omnis? At nam voluptates aperiam.
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+    </div>
   )
 }
 export default Review
