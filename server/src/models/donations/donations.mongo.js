e3bf43123d0d3a222ce9e39b6d03c805162af416
Aiyoun Khan <aiyounkhan@Aiyouns-Mac-mini.local>
2024-02-25 05:56:18 +0600

    Integration with add donation is added.

--- server/src/models/donations/donations.mongo.js
+++ server/src/models/donations/donations.mongo.js
@@ -25,7 +25,7 @@ const donationsSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    description: {
+    notes: {
         type: String,
         required: true
     },
